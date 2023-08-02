698579de69bccd57c09dbbd9c624a67cec419a57
Saad Ansari <ansarisaad175@gmail.com>
2023-08-02 17:29:46 +0530

    New changes

--- script.js
+++ script.js
@@ -1,70 +1,59 @@
-var selected = "";
 var dict = { rock: "✊", paper: "✋", scissor: "✌️" };
+var won = 0;
+var loss = 0;
+var tie = 0;
 
-function action(PlayerChoice) {
-  selected = PlayerChoice;
-  all = document.getElementsByClassName("action");
-  for (let index = 0; index < all.length; index++) {
-    all[index].style.backgroundColor = "black";
-  }
-  document.getElementById(PlayerChoice).style.backgroundColor = "rgb(95,95,95)";
-}
+function action(personchoice) {
+  final = document.getElementById("result1");
 
-function compchoice() {
-  number = Math.floor(Math.random() * 3);
+  compchoice = Math.floor(Math.random() * 3);
   const hand = ["✊", "✋", "✌️"];
-  return hand[number];
-}
+  comp = hand[compchoice];
 
-function getKeyByValue(object, value) {
-  return Object.keys(object).find((key) => object[key] === value);
-}
-
-function decide() {
-  if (selected != "") {
-    comp = compchoice();
-    document.getElementById(
-      "player_result"
-    ).innerHTML = `You chose:${dict[selected]}`;
-    document.getElementById("comp_result").innerHTML = `Comp chose:${comp}`;
-
-    final = document.getElementById("result");
-    compchoose = comp;
-    comp = getKeyByValue(dict, compchoose);
-
-    if (selected == "scissor") {
-      if (comp == "scissor") {
-        final.innerHTML = "Draw!";
-      } else if (comp == "rock") {
-        final.innerHTML = "You lost!";
-      } else {
-        final.innerHTML = "You Won!";
-      }
-    } else if (selected == "rock") {
-      if (comp == "scissor") {
-        final.innerHTML = "You Won!";
-      } else if (comp == "rock") {
-        final.innerHTML = "Draw!";
-      } else {
-        final.innerHTML = "You lost!";
-      }
+  if (dict[personchoice] == "✌️") {
+    if (comp == "✌️") {
+      final.innerHTML = "Tie.<br>You✌️✌️Computer";
+      tie++;
+    } else if (comp == "✊") {
+      final.innerHTML = "You lost.<br>You✌️✊Computer";
+      loss++;
     } else {
-      if (comp == "scissor") {
-        final.innerHTML = "You lost!";
-      } else if (comp == "rock") {
-        final.innerHTML = "You Won!";
-      } else {
-        final.innerHTML = "Draw!";
-      }
+      final.innerHTML = "You Won.<br>You✌️✋Computer";
+      won++;
     }
-
-    all = document.getElementsByClassName("action");
-    for (let index = 0; index < all.length; index++) {
-      all[index].style.backgroundColor = "black";
+  } else if (dict[personchoice] == "✊") {
+    if (comp == "✌️") {
+      final.innerHTML = "You Won.<br>You✊✌️Computer";
+      won++;
+    } else if (comp == "✊") {
+      final.innerHTML = "Tie.<br>You✊✊Computer";
+      tie++;
+    } else {
+      final.innerHTML = "You lost.<br>You✊✋Computer";
+      loss++;
     }
-    selected = "";
+  } else {
+    if (comp == "✌️") {
+      final.innerHTML = "You lost.<br>You✋✌️Computer";
+      loss++;
+    } else if (comp == "✊") {
+      final.innerHTML = "You Won.<br>You✋✊Computer";
+      won++;
+    } else {
+      final.innerHTML = "Tie.<br>You✋✋Computer";
+      tie++;
+    }
   }
+  document.getElementById(
+    "score"
+  ).innerHTML = `Wins: ${won}, Losses: ${loss}, Ties:${tie}`;
 }
-function start() {
-  document.getElementById("popup").style.display = "none";
+function reset() {
+  won = 0;
+  loss = 0;
+  tie = 0;
+  document.getElementById(
+    "score"
+  ).innerHTML = `Wins: ${won}, Losses: ${loss}, Ties:${tie}`;
+  document.getElementById("result1").innerHTML = "";
 }
